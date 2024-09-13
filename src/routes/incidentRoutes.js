6547b150679a5e0f7c9feef8eabcfc6a873e6fe5
mauricioenriquec <car21072@byui.edu>
2024-09-13 00:18:13 -0400

    new routes

--- src/routes/incidentRoutes.js
+++ src/routes/incidentRoutes.js
@@ -7,7 +7,6 @@ const router = express.Router();
 
 router.post('/', authMiddleware, upload.array('images', 3), async (req, res) => {
   try {
-
     const { title, description, location } = req.body;
     const images = req.files.map(file => file.path); 
     const user_id = req.user.id; 
@@ -66,6 +65,49 @@ router.get('/:id', authMiddleware, async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(400).send('Error al obtener la incidencia');
+  }
+});
+
+router.put('/:id', authMiddleware, async (req, res) => {
+  try {
+    const { status } = req.body;
+    const role = req.user.role;
+
+    if (role !== 'admin') {
+      return res.status(403).send('No tienes permiso para actualizar esta incidencia');
+    }
+
+    const incident = await Incident.updateStatus(req.params.id, status);
+
+    if (!incident) {
+      return res.status(404).send('Incidencia no encontrada');
+    }
+
+    res.status(200).send('Estado de la incidencia actualizado exitosamente');
+  } catch (error) {
+    console.error('Error al actualizar la incidencia:', error);
+    res.status(500).send('Error al actualizar la incidencia');
+  }
+});
+
+router.delete('/:id', authMiddleware, async (req, res) => {
+  try {
+    const role = req.user.role;
+
+    if (role !== 'admin') {
+      return res.status(403).send('No tienes permiso para eliminar esta incidencia');
+    }
+
+    const incident = await Incident.delete(req.params.id);
+
+    if (!incident) {
+      return res.status(404).send('Incidencia no encontrada');
+    }
+
+    res.status(200).send('Incidencia eliminada exitosamente');
+  } catch (error) {
+    console.error('Error al eliminar la incidencia:', error);
+    res.status(500).send('Error al eliminar la incidencia');
   }
 });
 
